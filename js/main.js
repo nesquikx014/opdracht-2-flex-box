0e40549e13c5c421fcc644c0bb292385fd338cfd
nesquikx014 <n.eroglux014@gmail.com>
2025-03-24 15:37:45 +0100

    ja

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const catchButton = document.getElementById("js--catch-button");
 const pokemonText = document.getElementById("js--pokemon-text");
 let pokemonGamePlayed = false;
 
-catchButton.onclick = function(){
+catchButton.onclick = function(event){
     if(pokemonGamePlayed === false){
         let catchNumber = Math.floor(Math.random() * 2); // o - 1
         if(catchNumber === 0){
@@ -27,3 +27,22 @@ catchButton.onclick = function(){
     pokemonGamePlayed = true;
     }
 }
+
+let inputField = document.getElementById("js--input");
+
+inputField.onkeyup = function(event){
+    let name = inputField.value;
+
+    if(event.keyCode === 13){
+        console.log(inputField.value);
+        let age = fetch("https://api.agify.io?name=")
+.then(function(response){
+    return response.json();
+})
+.then(function(echteData){
+    console.log(echteData);
+});
+
+    }
+}
+
